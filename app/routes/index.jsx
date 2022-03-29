a764dd9d7d2c5bbc931db401f801791b7a910ec9
Deric <derickdd716@gmail.com>
2022-03-29 14:25:11 -0400

    Add lat lon input field server-side validation

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,10 +2,35 @@ import { json, useLoaderData } from "remix";
 
 import { API_KEY } from "../utils/env.server";
 
+function validateLatLonParam(coordinate) {
+  if (isNaN(coordinate)) {
+    return `The coordinate is not a valid coordinate`;
+  }
+}
+
+const badRequest = (data) => {
+  return json(data, { status: 400 });
+};
+
 export const loader = async ({ request }) => {
   const url = new URL(request.url);
   const lat = url.searchParams.getAll("latitude");
   const lon = url.searchParams.getAll("longitude");
+
+  const fieldErrors = {
+    lat: validateLatLonParam(lat),
+    lon: validateLatLonParam(lon),
+  };
+
+  const fields = {
+    lat,
+    lon,
+  };
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({ fieldErrors, fields });
+  }
+
   const res = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`
   );
@@ -21,20 +46,52 @@ export default function Index() {
       <form method="get">
         <p>
           <label>
-            Latitude: <input name="latitude" type="text" />
+            Latitude:(required)
+            <input
+              defaultValue={weatherInfo?.fields?.lat}
+              name="latitude"
+              type="text"
+              aria-invalid={Boolean(weatherInfo?.fieldErrors?.lat) || undefined}
+              aria-errormessage={
+                weatherInfo?.fieldErrors?.lat ? "lat-error" : undefined
+              }
+            />
           </label>
+          {weatherInfo?.fieldErrors?.lat ? (
+            <p className="form-validation-error" role="alert" id="lat-error">
+              {weatherInfo.fieldErrors.lat}
+            </p>
+          ) : null}
         </p>
         <p>
           <label>
-            Longitude: <input name="longitude" type="text" />
+            Longitude: (required)
+            <input
+              defaultValue={weatherInfo?.fields?.lon}
+              name="longitude"
+              type="text"
+              aria-invalid={Boolean(weatherInfo?.fieldErrors?.lon) || undefined}
+              aria-errormessage={
+                weatherInfo?.fieldErrors?.lon ? "lon-error" : undefined
+              }
+            />
           </label>
+          {weatherInfo?.fieldErrors?.lon ? (
+            <p className="form-validation-error" role="alert" id="lon-error">
+              {weatherInfo.fieldErrors.lon}
+            </p>
+          ) : null}
         </p>
         <p>
-          <button type="submit">get weather</button>
+          <button type="submit">submit</button>
         </p>
       </form>
       <div>Hello Weather</div>
-      <div>It is currently {temp === null ? "Loading..." : temp} degrees F</div>
+      {weatherInfo?.fieldErrors ? (
+        <p>Please fix form errors</p>
+      ) : (
+        <p>It is currently {temp === null ? "Loading..." : temp} degrees F</p>
+      )}
     </div>
   );
 }
